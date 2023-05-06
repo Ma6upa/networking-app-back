235a9ead9f9eca76ef8b5a8a466f1ea27d2ecc1c
Ma6upa <vaud55@gmail.com>
2023-05-06 22:29:46 +0400

    add connection to mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
-const PORT = config.get('port') || 5000
+const PORT = config.get('port')
 
-app.listen(PORT, () => console.log('app working on port', PORT))
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'))
+    app.listen(PORT, () => console.log('app working on port', PORT))
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
+}
+
+start()
