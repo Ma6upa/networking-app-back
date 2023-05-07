404129864b1bf33260ddfd8a6db6ef10b9eb44c5
Ma6upa <vaud55@gmail.com>
2023-05-07 19:35:54 +0400

    add necessary middleware to parse req.body to json

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = config.get('port')
 
+app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 
 async function start() {
