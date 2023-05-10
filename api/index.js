e47429870aa76bddbd8b0d8760b5e58f873276a3
Ma6upa <vaud55@gmail.com>
2023-05-10 23:27:33 +0400

    update to publish to vercel

--- api/index.js
+++ api/index.js
@@ -6,9 +6,9 @@ const app = express()
 const PORT = config.get('port')
 
 app.use(express.json({ extended: true }))
-app.use('/api/auth', require('./routes/auth.routes'))
-app.use('/api/user', require('./routes/user.routes'))
-app.use('/api/posts', require('./routes/posts.routes'))
+app.use('/api/auth', require('../routes/auth.routes'))
+app.use('/api/user', require('../routes/user.routes'))
+app.use('/api/posts', require('../routes/posts.routes'))
 app.use(express.static(__dirname + '/images'));
 
 async function start() {
